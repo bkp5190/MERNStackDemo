894f8425ad44833afb789de337d415ab6179d142
Boney Patel <boneypatel37@yahoo.com>
2023-07-06 09:04:30 -0400

    Showing decks on landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+type TDeck = {
+  title: string;
+  _id: string;
+}
+
+
 function App() {
+  const [decks, setDecks] = useState<TDeck[]>([]);
   const [title, setTitle] = useState('');
 
   async function handleCreateDeck(e: React.FormEvent) {
@@ -20,7 +27,23 @@ function App() {
     setTitle("");
   }
 
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch('http://localhost:5001/decks');
+      const newDecks = await response.json();
+      setDecks(newDecks);
+    }
+    fetchDecks();
+  }, []);
+
   return <div className='App'>
+    <ul className="decks">
+      {
+        decks.map((deck) => (
+          <li key={deck._id}>{deck.title}</li>
+        ))
+      }
+    </ul>
     <form onSubmit={handleCreateDeck}>
       <label htmlFor='deck-title'>Deck Title</label>
       <input 
