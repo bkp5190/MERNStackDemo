e808b76fd63d03273095f5203aef140901172503
Boney Patel <boneypatel37@yahoo.com>
2023-07-06 01:18:23 -0400

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+const PORT = 5001;
+
+app.use(express.json())
+
+app.post('/decks', async (req: Request, res: Response) => {
+    const newDeck = new Deck({
+        title: req.body.title,
+    });
+    const createdDeck = await newDeck.save();
+    res.json(createdDeck);
+})
+
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+    console.log(`Listening on port ${PORT}`)
+    app.listen(PORT);
+})
