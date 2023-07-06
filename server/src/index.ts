74aaa653eb36f6141818878304417e23ea47e12b
Boney Patel <boneypatel37@yahoo.com>
2023-07-06 11:23:51 -0400

    Card creation

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import { config } from 'dotenv';
 import { getDecksCotroller } from "./controllers/getDeckController";
 import { createDeckController } from "./controllers/createDeckController";
 import { deleteDeckController } from "./controllers/deleteDeckController";
+import { createCardForDeckController } from "./controllers/createCardForDeckController";
 
 config();
 
@@ -21,6 +22,8 @@ app.use(express.json());
 app.get('/decks', getDecksCotroller);
 app.post('/decks', createDeckController);
 app.delete('/decks/:deckId', deleteDeckController);
+
+app.post('/decks/:deckId/cards', createCardForDeckController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`Listening on port ${PORT}`)
