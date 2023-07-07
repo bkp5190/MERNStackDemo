45b8e47058a78bc6bc14bef00a48b1c963dd4d12
Boney Patel <boneypatel37@yahoo.com>
2023-07-07 18:43:13 -0400

    Fix bugs, refactor code, card apis

--- client/src/api/createCard.ts
+++ client/src/api/createCard.ts
@@ -1,7 +1,6 @@
 import { API_URL } from "./config";
-import { TDeck } from "./getDecks";
 
-export async function createCard(deckId: string, text: string): Promise<TDeck> {
+export async function createDeck(deckId: string, text: string) {
   const response = await fetch(`${API_URL}/decks/${deckId}/cards`, {
     method: "POST",
     body: JSON.stringify({
